2067d1a9465e5f26873b66edd52fadc1f1801db0
Sanjeet Kumar <anjeesanjeet@gmail.com>
2025-10-04 01:14:52 +0530

    file structure completed

---
+++ template.sh
@@ -0,0 +1,11 @@
+mkdir backend
+
+touch backend/ai_agent.py
+touch backend/main.py
+touch backend/config.py
+touch backend/tools.py
+touch requirements.txt
+touch frontend.py
+touch app.py
+
+echo "Project structure created successfully."
